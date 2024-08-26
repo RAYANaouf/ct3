3549785141eadd52fac10d20c37615a00ee53228
Rayan Aouf <rayanaouf1512@gmail.com>
2024-08-26 15:02:47 +0100

    update chiffrage doctype

--- ct3/ct3/doctype/chiffrage/chiffrage.js
+++ ct3/ct3/doctype/chiffrage/chiffrage.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, rayan aouf and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Chiffrage", {
-// 	refresh(frm) {
+frappe.ui.form.on("Chiffrage", {
+	refresh(frm) {
+		//frappe.msgprint("ow");
+	},
+})
 
-// 	},
-// });
+
+
+
+
+frappe.ui.form.on("Chiffrage_BOQ" , {
+
+	boq_items_add : function(frm){
+		frappe.msgprint("jhgfgh");
+	}
+
+})
